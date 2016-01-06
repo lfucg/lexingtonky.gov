151cd7308a5101c8e2fbef8f1599141ae8bbe4f5
Erik Schwartz <erik@erikschwartz.net>
2016-01-06 15:38:30 -0500

    Escape host urls

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -28,9 +28,9 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 $settings['trusted_host_patterns'] = array(
-  '^dev-lexky-d8.pantheon.io$',
-  '^test-lexky-d8.pantheon.io$',
-  '^live-lexky-d8.pantheon.io$',
+  '^dev-lexky-d8\.pantheon\.io$',
+  '^test-lexky-d8\.pantheon\.io$',
+  '^live-lexky-d8\.pantheon\.io$',
   '^lexingtonky\.gov$',
   '^www\.lexingtonky\.gov$',
 );
