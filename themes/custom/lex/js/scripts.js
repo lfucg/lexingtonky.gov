d4c4bfa47f09fbc8931c4ba14164dfad36ddc197
Erik Schwartz <erik@erikschwartz.net>
2016-01-20 15:31:25 -0500

    Fix JS error: check for feedback link, redirect feedback to /

--- themes/custom/lex/js/scripts.js
+++ themes/custom/lex/js/scripts.js
@@ -1,5 +1,7 @@
 (function() {
   // set feedback_url
   var link = document.getElementById('js-pagefeedback-link');
-  link.setAttribute('href', link.getAttribute('href') + '?feedback_url=' + window.location.pathname);
+  if (link) {
+    link.setAttribute('href', link.getAttribute('href') + '?feedback_url=' + window.location.pathname);
+  }
 }());
