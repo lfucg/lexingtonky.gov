00519dd61c1ff7dd35faf2934b8deaacc9d1e766
Erik Schwartz <erik@erikschwartz.net>
2016-02-01 17:15:28 -0500

    Add broken zombie.js test driver

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,6 +5,7 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
+
 /**
  * Defines application features from the specific context.
  */
