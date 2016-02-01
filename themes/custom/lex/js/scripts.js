fa48159866f6f14c0ba5ce0932cb1780bef66ef2
Erik Schwartz <erik@erikschwartz.net>
2016-02-01 14:43:17 -0500

    WIP

--- themes/custom/lex/js/scripts.js
+++ themes/custom/lex/js/scripts.js
@@ -1,7 +1,19 @@
 (function() {
   // set feedback_url
+  var $ = jQuery;
   var link = document.getElementById('js-pagefeedback-link');
   if (link) {
     link.setAttribute('href', link.getAttribute('href') + '?feedback_url=' + window.location.pathname);
   }
+
+  $('#contact-message-page-feedback-form').submit(function(e) {
+    e.preventDefault();
+    var form = $(e.target);
+    $.ajax({
+      type:'POST',
+      url: '/contact/page_feedback',
+      data: form.serialize(),
+      success: function(response) {
+    }});
+  })
 }());
