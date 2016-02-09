13aa1023a900846aca79259aa00ccd50a4a1bd48
Matt Smith <mtsmit2@gmail.com>
2016-02-09 14:38:42 -0500

    Unzip the downloaded file

--- scripts/ci-database-override.sh
+++ scripts/ci-database-override.sh
@@ -5,5 +5,6 @@ cd ./db_backup
 
 terminus auth login --machine-token=$MACHINE_TOKEN 2> /dev/null
 terminus site backups get --site=$SITE_NAME --env=live --element=database --to=. --latest
+gunzip "./`ls | head -1`"
 mysql -u ubuntu circle_test < "./`ls | head -1`"
 
