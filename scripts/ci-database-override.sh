0d1c95c1d4a5d9a7c8e564590cb57bdc8879bfd1
Matt Smith <mtsmit2@gmail.com>
2016-02-09 14:30:10 -0500

    Update to paths used in database script

--- scripts/ci-database-override.sh
+++ scripts/ci-database-override.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 mkdir "./db_backup"
+cd ./db_backup
 
 terminus auth login --machine-token=$MACHINE_TOKEN 2> /dev/null
-terminus site backups get --site=$SITE_NAME --env=live --element=database --to=./db_backup --latest
-mysql -u ubuntu circle_test < "./db_backup/`ls | head -1`"
+terminus site backups get --site=$SITE_NAME --env=live --element=database --to=. --latest
+mysql -u ubuntu circle_test < "./`ls | head -1`"
 
