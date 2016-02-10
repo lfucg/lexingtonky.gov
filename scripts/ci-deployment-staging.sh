f5df62bedb4099bae0657f4d655a1a52d4638ba5
Matt Smith <mtsmit2@gmail.com>
2016-02-09 19:22:18 -0500

    add quotes around drush command

--- scripts/ci-deployment-staging.sh
+++ scripts/ci-deployment-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 terminus site deploy --site=$SITE_NAME --env=test --updatedb --cc --sync-content
-terminus drush cim -y --site=$SITE_NAME --env=test
+terminus drush "cim -y" --site=$SITE_NAME --env=test
