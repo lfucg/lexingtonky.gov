758270934daef8f2836d23877c5ca86ec5fb808a
Matt Smith <mtsmit2@gmail.com>
2016-02-09 20:15:13 -0500

    Typo fix

--- scripts/ci-deployment-staging.sh
+++ scripts/ci-deployment-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-terminus site deploy --site=$SITE_NAME --env=test --updatedb --cc --sync-content -note="Deployed from CirceCI"
+terminus site deploy --site=$SITE_NAME --env=test --updatedb --cc --sync-content --note="Deployed from CirceCI"
 terminus drush "cim -y" --site=$SITE_NAME --env=test
