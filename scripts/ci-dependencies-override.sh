fd88b37856db82cee780d2fea9c67bb52fc0a034
Matt Smith <mtsmit2@gmail.com>
2016-02-09 20:04:23 -0500

    Use pantheon-systems/cli:dev-master until deployment bug is released

--- scripts/ci-dependencies-override.sh
+++ scripts/ci-dependencies-override.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-composer global require pantheon-systems/cli --prefer-source --no-interaction
+composer global require pantheon-systems/cli:dev-master --prefer-source --no-interaction
 composer require drush/drush:8 --prefer-source --no-interaction
 composer install --prefer-source --no-interaction
