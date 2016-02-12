3561c543c4187219373f262f10865fb2b0e9ddb2
Erik Schwartz <erik@erikschwartz.net>
2016-02-12 11:51:39 -0500

    Exit deploy on any error

--- private/scripts/deploy-production.sh
+++ private/scripts/deploy-production.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# exit immediately if any command fails
+set -e
+
 terminus site backups create --env=live --element=all
 terminus site deploy --env=live --updatedb --cc
-terminus drush cim -y --env=live`
+terminus drush cim -y --env=live
 
