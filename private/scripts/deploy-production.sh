84d009a3a7d6c4bd5bec5a2d3d51738270692b14
Erik Schwartz <erik@erikschwartz.net>
2016-02-18 15:20:11 -0500

    Fix deploy script syntax

--- private/scripts/deploy-production.sh
+++ private/scripts/deploy-production.sh
@@ -4,6 +4,6 @@
 set -e
 
 terminus site backups create --env=live --element=all
-terminus site deploy --env=live --updatedb --cc
-terminus drush cim -y --env=live
+terminus site deploy --env=live --updatedb --cc --note="Deployed from local env"
+terminus drush "cim -y" --env=live
 
