b00044e044cf7dc93106a9abdee87a6c42bf841b
Erik Schwartz <erik@erikschwartz.net>
2016-03-21 16:32:49 -0400

    Aggregate JS, add echo banners during deploy

--- private/scripts/deploy-production.sh
+++ private/scripts/deploy-production.sh
@@ -3,7 +3,20 @@
 # exit immediately if any command fails
 set -e
 
+
+banner()
+{
+  echo "*****************"
+  echo "*** $1 ***"
+  echo "*****************"
+}
+
+banner "creating backup"
 terminus site backups create --env=live --element=all
+
+banner "Deplying code"
 terminus site deploy --env=live --updatedb --cc --note="Deployed from local env"
+
+banner "Importing configuration"
 terminus drush "cim -y" --env=live
 
