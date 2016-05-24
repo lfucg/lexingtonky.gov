380d86018976bca7db3a7c2c2ad2a4234eab9954
Erik Schwartz <erik@erikschwartz.net>
2016-05-24 11:25:49 -0400

    Run drush updatedb during CI install

--- private/scripts/ci-test-pre.sh
+++ private/scripts/ci-test-pre.sh
@@ -14,6 +14,7 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then terminus site create-env --site=$SITE_
 terminus sites aliases
 terminus site clone-content --site=$SITE_NAME --from-env=live --to-env=$SITE_ENV --yes
 terminus drush "cim -y" --site=$SITE_NAME --env=$SITE_ENV 2> /dev/null
+terminus drush "updatedb -y" --site=$SITE_NAME --env=$SITE_ENV 2> /dev/null
 terminus drush "pm-enable -y devel" --site=$SITE_NAME --env=$SITE_ENV
 terminus drush "config-set -y system.mail interface.default devel_mail_log" --site=$SITE_NAME --env=$SITE_ENV
 terminus drush cr --site=$SITE_NAME --env=$SITE_ENV --yes 2> /dev/null
