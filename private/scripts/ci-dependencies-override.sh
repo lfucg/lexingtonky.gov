99ce8571c54bed3e204b5375db159629cc1280de
Erik Schwartz <erik@erikschwartz.net>
2016-06-17 14:41:33 -0400

    Set drupal-extension version
    Version 3.2.0 broke some tests

--- private/scripts/ci-dependencies-override.sh
+++ private/scripts/ci-dependencies-override.sh
@@ -3,6 +3,6 @@
 # add dev dependencies
 composer global require pantheon-systems/terminus:dev-master --no-interaction
 composer require drush/drush:"^8.1" --no-interaction
-composer require drupal/drupal-extension:"^3.1" --no-interaction
+composer require drupal/drupal-extension:"3.1.5" --no-interaction
 composer require guzzlehttp/guzzle:"^6.0@dev" --no-interaction
 composer require jarnaiz/behat-junit-formatter:"^1.3" --no-interaction
