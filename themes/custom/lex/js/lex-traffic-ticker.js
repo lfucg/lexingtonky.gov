6f806ce43459d30dc79d65cbf334c446a1ce35f9
Erik Schwartz <erik@erikschwartz.net>
2016-07-17 14:36:59 -0400

    Remove traffic ticker test content

--- themes/custom/lex/js/lex-traffic-ticker.js
+++ themes/custom/lex/js/lex-traffic-ticker.js
@@ -49,7 +49,7 @@
   var sectionHeading = function(firstRow) {
     return (firstRow.sectionHeading ?
       '<h2>' + firstRow.sectionHeading + '</h2>' :
-      'Nothing');
+      '');
   }
 
   var displayIncidents = function(incidents) {
