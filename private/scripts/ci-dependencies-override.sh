130de3d328ae277e517be3e3d8870478adef60d4
Erik Schwartz <erik@erikschwartz.net>
2016-07-17 16:06:12 -0400

    Revert to ubuntu 12.04 circleci settings

--- private/scripts/ci-dependencies-override.sh
+++ private/scripts/ci-dependencies-override.sh
@@ -4,5 +4,4 @@
 composer global require pantheon-systems/terminus:dev-master --no-interaction
 composer require drush/drush:"^8.1" --no-interaction
 composer require drupal/drupal-extension:"3.1.5" --no-interaction
-composer require guzzlehttp/guzzle:"^6.0@dev" --no-interaction
 composer require jarnaiz/behat-junit-formatter:"^1.3" --no-interaction
