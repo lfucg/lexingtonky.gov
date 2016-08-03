6641928dc9422872e47eca6ff6818b018aa97007
Erik Schwartz <erik@erikschwartz.net>
2016-08-03 16:15:39 -0400

    Add 301 redirects

--- sites/default/redirects.php
+++ sites/default/redirects.php
@@ -52,17 +52,30 @@ function _lexky_desired_host() {
   }
 }
 
-function _lexky_redirect($new_path) {
-  $status = '302 Moved Temporarily';
+function _lexky_status_permanent() {
+  return '301 Moved Permanently';
+}
+
+function _lexky_status_temporary() {
+  return '302 Moved Temporarily';
+}
+
+function _lexky_internal_redirect_status($current_path) {
+  if (strpos($current_path, '/index.aspx') === 0) {
+    return _lexky_status_permanent();
+  }
+  return _lexky_status_temporary();
+}
+
+function _lexky_redirect($new_path, $http_status) {
   if (strpos($new_path, 'http') === 0) {
     $new_url = $new_path;
   } else {
-    // todo: use 301 for index.aspx urls
     $new_url = _lexky_desired_http() . '://' . _lexky_desired_host() . $new_path;
   }
 
   if (_lexky_is_sane_redirect($new_url)) {
-    header('HTTP/1.0 ' . $status);
+    header('HTTP/1.0 ' . $http_status);
     header('Location: ' . $new_url);
     exit();
   }
@@ -87,10 +100,10 @@ $incoming_path = $_SERVER['REQUEST_URI'];
 $redirect_table_path = _lexky_get_redirect_from_table($incoming_path);
 
 if ($redirect_table_path) {
-  _lexky_redirect($redirect_table_path);
+  _lexky_redirect($redirect_table_path, _lexky_internal_redirect_status($incoming_path));
 } else if (in_array($_SERVER['HTTP_HOST'], ['next.lexingtonky.gov', 'lexingtonky.gov'])) {
   // redirect to www.lexingtonky.gov
-  _lexky_redirect($incoming_path);
+  _lexky_redirect($incoming_path, _lexky_status_permanent());
 } else if (_lexky_pantheon_http()) {
-  _lexky_redirect($_SERVER['REQUEST_URI']);
+  _lexky_redirect($_SERVER['REQUEST_URI'], _lexky_status_permanent());
 }
