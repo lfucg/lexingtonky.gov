052c5c5d8e6b39da8b11c14fe0ee56f204bbe628
Dustin Peerce <dustin.a.peerce@gmail.com>
2016-08-27 05:36:16 -0400

    Add absolute file path

--- modules/custom/tests/AddtocalControllerTest.php
+++ modules/custom/tests/AddtocalControllerTest.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once('../addtocal/src/Controller/AddtocalController.php');
+require_once(__DIR__ . '/../addtocal/src/Controller/AddtocalController.php');
 
 class AddtocalControllerTest extends PHPUnit_Framework_TestCase {
 
 	public function testFormatDate() {
 		$pages = new AddtocalController();
-		
+
 		$expected = '20111027T202339Z';
 
 		$this->assertEquals($expected, $pages->formatDate('2011-10-27T20:23:39'));
