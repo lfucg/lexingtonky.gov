3399e87465c4f0e13d130ee7eed210f419434116
Dustin Peerce <dustin.a.peerce@gmail.com>
2016-08-27 04:41:37 -0400

    Attempt to fix php parse error

--- modules/custom/tests/AddtocalControllerTest.php
+++ modules/custom/tests/AddtocalControllerTest.php
@@ -4,9 +4,9 @@ require_once('../addtocal/src/Controller/AddtocalController.php');
 
 class AddtocalControllerTest extends PHPUnit_Framework_TestCase {
 
-	$pages = new AddtocalController();
-
 	public function testFormatDate() {
+		$pages = new AddtocalController();
+		
 		$expected = '20111027T202339Z';
 
 		$this->assertEquals($expected, $pages->formatDate('2011-10-27T20:23:39'));
