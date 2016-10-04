e04f6de806b0a581eb6d43c9a4f486114fb20fe5
Erik Schwartz <erik@erikschwartz.net>
2016-10-04 13:50:34 -0400

    Add background for police recruitment sibling pages

---
+++ themes/custom/lex/js/lex-sidenav-list.js
@@ -0,0 +1,6 @@
+(function() {
+  var $ = jQuery;
+  /* add this class in the view when this block expands beyond recruitment */
+  var klass = 'lex-sidenav-list-policerecruitment';
+  $('.js-main').addClass(klass);
+}());
