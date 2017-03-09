712da61983f9dda005496a50d98d232403fc2775
LEXUCG\mmorris <mmorris@lexingtonky.gov>
2017-03-09 08:18:26 -0500

    Switchable Themes based on related department.

---
+++ modules/lex_themecontrol/src/LexThemeNegotiator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\lex_themecontrol;
+
+use Drupal\Core\Theme\DefaultNegotiator;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\NodeInterface;
+
+/**
+ * Switches Theme according to the related department of the site.
+ */
+class LexThemeNegotiator extends DefaultNegotiator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    $node = $route_match->getParameter('node');
+
+    if ($node instanceof NodeInterface) {
+      try {
+        $departments = $node->get('field_related_departments')->getValue();
+
+        if (is_array($departments)) {
+          foreach ($departments as $department) {
+            switch($department['target_id']) {
+              case 2: return 'lex_police';
+            }
+          }
+        }
+      }
+      catch ( \InvalidArgumentException $e) {}
+    }
+
+    return NULL;
+  }
+
+}
