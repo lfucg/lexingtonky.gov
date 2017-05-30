fe7afe1590ef9b805c799319788cd785eb272f71
Michael Morris <mmorris@lexingtonky.gov>
2017-05-30 12:10:00 -0400

    Search screen formatting.

--- themes/custom/lex/js/scripts.js
+++ themes/custom/lex/js/scripts.js
@@ -60,6 +60,17 @@
   /* duplicated in browse-columns.js, lex-gis.js */
   $.LexingtonFilterBlock(document.getElementsByClassName('js-lex-filter-block')[0]);
 
+  console.log('cache busted');
+  // Now correct titles and search terms on the search pages.
+  if ($('body.path-search')) {
+    var search = getParameterByName('search_api_fulltext');
+console.log(search);
+    if (search) {
+      $('input[type="search"]').val(search);
+      $('h1').append(' for "' + search + '"');
+    }
+  }
+
 }(jQuery));
 
 
@@ -95,4 +106,14 @@ function googleTranslateElementInit() {
   if (label && ! label.innerHTML.match('Translate')) {
     label.innerHTML = '<span class="lex-translatelink">Translate</span>';
   }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
