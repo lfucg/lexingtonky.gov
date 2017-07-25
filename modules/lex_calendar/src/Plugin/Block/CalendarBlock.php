9c56557b237dcc3be2aae1e01dcc0ce11efe5c99
tendoAki <tendoAki@gmail.com>
2017-07-25 08:24:29 -0400

    Disable caching!

--- modules/lex_calendar/src/Plugin/Block/CalendarBlock.php
+++ modules/lex_calendar/src/Plugin/Block/CalendarBlock.php
@@ -202,6 +202,7 @@ class CalendarBlock extends BlockBase implements BlockPluginInterface, Container
         '#theme' => 'lex_calendar_event_block',
         '#dates' => $dates,
         '#content_type' => $this->contentType,
+        # Force Git to detect change to next line.
         '#cache' => ['max-age' => 0]
       ];
     }
