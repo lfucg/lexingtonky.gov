6e462df1a82363584d85116c148e8868c114183b
JMStewart00 <josh@apaxsoftware.com>
2019-11-06 07:37:03 -0500

    Clean up of a few files, fixed councilmembers search

--- themes/custom/lex/js/takeover-navigation.js
+++ themes/custom/lex/js/takeover-navigation.js
@@ -11,13 +11,5 @@
     $(".fade").toggleClass('show');
     $("body").toggleClass('no-scroll');
     $('.menu-label').html($('.menu-label').html() == 'Menu' ? 'Close' : 'Menu');
-
-
-    // going to need to use this for nav set up.
-    if (windowWidth > 998) {
-      console.log('bigSCREENS');
-    } else {
-      console.log('littlescreens');
-    }
   });
 }(jQuery));
