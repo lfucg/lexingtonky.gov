d4411348b3bdc3985aa7c36d7747679cce7950a4
Jacob Hall <jacobhall@Jacobs-Macbook-Pro.local>
2019-12-16 08:54:26 -0500

    Calendar rework for desktop and mobile view

---
+++ themes/custom/lex/js/lex-cal.js
@@ -0,0 +1,34 @@
+(function () {
+    var $ = jQuery;
+
+    $('.list-switch').click(function () {
+        console.log('hey');
+        $('.list-switch').css({
+            'background-color': 'white',
+            'color': '#004585'
+        });
+        $('.month-switch').css({
+            'background-color': '#EFEFEF',
+            'color': '#353535'
+        });
+        $('#sidebar-calendar').css({
+            'display': 'block',
+            'opacity': '100%'});
+        $('#calendar').css('display', 'none');
+        $('.calendar-key').css('display', 'none');
+    });
+
+    $('.month-switch').click(function () {
+        $('.month-switch').css({
+            'background-color': 'white',
+            'color': '#004585'
+        });
+        $('.list-switch').css({
+            'background-color': '#EFEFEF',
+            'color': '#353535'
+        });
+        $('#sidebar-calendar').css('display', 'none');
+        $('#calendar').css('display', 'block');
+        $('.calendar-key').css('display', 'block');
+    });
+}());
