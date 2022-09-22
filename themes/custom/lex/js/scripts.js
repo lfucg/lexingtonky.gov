cabf1c217fb26bc6e0a53a3500ad9b9dcbde3e25
Taylor <taylorakin.ky@gmail.com>
2022-09-22 13:47:07 -0400

    LG-61 Fixes validation for front page feedback form
     - comments out code in scripts.js, which sercumvented the capthca validation
     - updated honeypot config
     - moved status messages block to top of lex_home block layout

--- themes/custom/lex/js/scripts.js
+++ themes/custom/lex/js/scripts.js
@@ -2,34 +2,36 @@
 (function($) {
   // $('.lex-hide-unless-javascript').removeClass('lex-hide-unless-javascript');
 
-  $('#js-feedback-button').find('a').click(function () {
-    $('.lex-pagefeedback-container').find('.js-accordion-control').click();
-  });
+  // $('#js-feedback-button').find('a').click(function () {
+  //   $('.lex-pagefeedback-container').find('.js-accordion-control').click();
+  // });
 
-  $('.js-accordion-content #contact-message-page-feedback-form').submit(function(e) {
-    e.preventDefault();
-    var form = $(e.target);
-    var submitBtn = form.find("input[type=submit]:visible");
-    var accordion = form.parent('.js-accordion-content');
-    var feedback_url = window.location.pathname + window.location.search;
+  // $('.js-accordion-content #contact-message-page-feedback-form').submit(function(e) {
+  //   e.preventDefault();
+  //   // var form = $(e.target);
+  //   var submitBtn = form.find("input[type=submit]:visible");
+  //   var accordion = form.parent('.js-accordion-content');
+  //   var feedback_url = window.location.pathname + window.location.search;
 
-    form.find('#edit-field-feedback-url-0-value').val(feedback_url);
-    submitBtnOriginalVal = submitBtn.val();
-    submitBtn.val('Sending...');
+  //   form.find('#edit-field-feedback-url-0-value').val(feedback_url);
+  //   submitBtnOriginalVal = submitBtn.val();
+  //   submitBtn.val('Sending...');
 
-    $.ajax({
-      type:'POST',
-      url: '/contact/page_feedback',
-      data: form.serialize(),
-      success: function(response) {
-        accordion.find('.js-success').removeClass('visually-hidden');
-        submitBtn.val(submitBtnOriginalVal);
-      },
-      error: function(response) {
-        accordion.find('.js-error').removeClass('visually-hidden');
-      }
-    });
-  });
+  //   $.ajax({
+  //     type:'POST',
+  //     url: '/contact/page_feedback',
+  //     data: form.serialize(),
+  //     success: function(response) {
+  //       console.log('success');
+  //       accordion.find('.js-success').removeClass('visually-hidden');
+  //       submitBtn.val(submitBtnOriginalVal);
+  //     },
+  //     error: function(response) {
+  //       console.log('error');
+  //       accordion.find('.js-error').removeClass('visually-hidden');
+  //     }
+  //   });
+  // });
 
   $.LexingtonFilterBlock = function(el) {
     var options = {
