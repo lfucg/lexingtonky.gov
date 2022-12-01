b6627d0639975a399336768707cfd3f352c00def
Chris Allen <chris@apaxsoftware.com>
2022-12-01 11:01:47 -0500

    Moved to /web docroot and pulled in latest pantheon settings

--- core/assets/vendor/jquery.ui/ui/version-min.js
+++
@@ -1,2 +0,0 @@
-!function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery"],e):e(jQuery)}((function(e){"use strict";return e.ui=e.ui||{},e.ui.version="1.13.1"}));
-//# sourceMappingURL=version-min.js.map
