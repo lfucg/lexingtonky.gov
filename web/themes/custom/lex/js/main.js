837ad3656fd17484ee5009d2c0859e9db87b9c75
Taylor <taylorakin.ky@gmail.com>
2022-12-23 18:16:58 -0500

    LG-29 Site Page Banners
     - Adds new content type "Alert"
     - adds new region to site themes
     - adds styling for alerts
     - new js file for handling cookies and displaying/hiding alerts
     - doesn't remove old site notices, need to do so at some point.
     - fixing fa_link plugin, "does not exisit"
     - unset fa link from being used on the node itself. Crappy solution
     - configured gulp file for scripts
     - adds commented out fontawesome library for later usage

---
+++ web/themes/custom/lex/js/main.js
@@ -0,0 +1,12 @@
+import './alerts'
+import './browse-columns';
+import './cards';
+import './internal-search-block';
+import './join-police';
+import './lex-cal';
+import './lex-geocoder--councilmembers';
+import './lex-neighborhood-associations';
+import './lex-sidenav-list';
+import './search';
+import './takeover-navigation';
+import './webdesignstandards-accordion';
